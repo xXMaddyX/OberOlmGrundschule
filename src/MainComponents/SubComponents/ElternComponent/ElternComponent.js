b3c77fd6ce8e222a1df0454b7f0459c5f9fe71ec
xXMaddyX <maddyaion@hotmail.de>
2025-12-06 15:32:31 +0100

    update Eltern-site

--- src/MainComponents/SubComponents/ElternComponent/ElternComponent.js
+++ src/MainComponents/SubComponents/ElternComponent/ElternComponent.js
@@ -1,4 +1,5 @@
 import MakeSubSiteNav from "../../../Singeltones/MakeSubSiteNavigation.js";
+import ElternComponentData from "./ElternComponentData.js";
 import ElternHTML from "./ElternHTML.js";
 
 export default class ElternComponent extends HTMLElement{
@@ -13,5 +14,82 @@ export default class ElternComponent extends HTMLElement{
         this.shadow.innerHTML = HTML;
 
         MakeSubSiteNav.Instance.createSiteNavigation(this);
+
+        this.initRefs();
+        this.render();
+    }
+
+    initRefs() {
+        this.sebClass = this.shadow.querySelector(".seb");
+    }
+
+    render() {
+        this.renderSEBCards();
+    }
+
+    renderSEBCards() {
+        ElternComponentData.forEach(({imageLink, headingH1, cardName, cardNameDes, cardList, cardFooterLine}) => {
+            /**@type {HTMLDivElement} */
+            const Card = document.createElement("div");
+            Card.classList.add("seb-card-container");
+
+            /**@type {HTMLImageElement} */
+            const CardImage = document.createElement("img");
+            CardImage.src = imageLink;
+
+            /**@type {HTMLHeadingElement} */
+            const CardHeading = document.createElement("h1");
+            CardHeading.innerText = headingH1;
+
+            /**@type {HTMLHeadingElement} */
+            const CardName = document.createElement("h2");
+            CardName.innerText = cardName;
+
+            /**@type {HTMLParagraphElement} */
+            const CardNameDes = document.createElement("p");
+            CardNameDes.innerText = cardNameDes;
+
+            const CardListElement = this.renderSevListElem(cardList);
+
+            /**@type {HTMLHeadingElement} */
+            const CardFooter = document.createElement("h2");
+            CardFooter.innerText = cardFooterLine;
+
+            Card.append(
+                CardImage,
+                CardHeading,
+                CardName,
+                CardNameDes,
+                CardListElement,
+                CardFooter
+            );
+            this.sebClass.append(Card);
+        })
+
+    }
+    renderSevListElem(cardList) {
+        /**@type {HTMLDivElement} */
+        const ListContainer = document.createElement("div");
+        ListContainer.classList.add("seb-card-list-elem")
+
+        /**@type {HTMLHeadingElement} */
+        const CardListHeading = document.createElement("h2");
+        CardListHeading.innerText = cardList.listHeading;
+
+        /**@type {HTMLUListElement} */
+        const ListBody = document.createElement("ul");
+
+        cardList.listPoints.forEach((item) => {
+            /**@type {HTMLLIElement} */
+            let listElem = document.createElement("li");
+            listElem.innerText = item;
+            ListBody.appendChild(listElem);
+        });
+
+        ListContainer.append(
+            CardListHeading,
+            ListBody,
+        );
+        return ListContainer;
     }
 }
---
+++ src/MainComponents/SubComponents/ElternComponent/ElternComponentData.js
@@ -0,0 +1,90 @@
+const ElternComponentData = [
+    {
+        imageLink: 'images/eltern-abc-img.webp',
+        headingH1: 'Schulelternsprecher',
+        cardName: 'Frank Ehresmann',
+        cardNameDes: '2 Kinder: Anicke (IGS) und Jonah, 3a',
+        cardList: {
+            listHeading: 'Aufgabenschwerpunkte im SEB',
+            listPoints: [
+                'Kontakt zur Schulleitung, Termin- und Sitzungsplanung',
+                'Kontarkt zum Schulträgerausschuss',
+                'Austausch zwischen KlassenelternSprechern, Förderverein und Schulelternbeirat',
+                'Verkehrssicherheit (sicherer Schulweg)',
+                'Koordination "Einschulungsfeier"',
+            ],
+        },
+        cardFooterLine: 'Mitglied im Schulausschuss/Gesamtkonferenz',
+    },
+    {
+        imageLink: 'images/eltern-abc-img.webp',
+        headingH1: 'Vertreterin des Schulelternsprechers',
+        cardName: 'Julia Schneider',
+        cardNameDes: '3 Kinder: Emma(IGS), Anton 4a und Frieda(KiGa)',
+        cardList: {
+            listHeading: 'Aufgabenschwerpunkt im SEB',
+            listPoints: [
+                'Kontakt zu Hausaufgabenbetreuern',
+                'Schulbuchausschuss'
+            ],
+        },
+        cardFooterLine: 'Mitglied im Schulausschuss/Gesamtkonferenz',
+    },
+    {
+        imageLink: 'images/eltern-abc-img.webp',
+        headingH1: '',
+        cardName: '',
+        cardNameDes: '',
+        cardList: {
+            listHeading: '',
+            listPoints: [
+                '',
+                ''
+            ],
+        },
+        cardFooterLine: '',
+    },
+    {
+        imageLink: 'images/eltern-abc-img.webp',
+        headingH1: '',
+        cardName: '',
+        cardNameDes: '',
+        cardList: {
+            listHeading: '',
+            listPoints: [
+                '',
+                ''
+            ],
+        },
+        cardFooterLine: '',
+    },
+    {
+        imageLink: 'images/eltern-abc-img.webp',
+        headingH1: '',
+        cardName: '',
+        cardNameDes: '',
+        cardList: {
+            listHeading: '',
+            listPoints: [
+                '',
+                ''
+            ],
+        },
+        cardFooterLine: '',
+    },
+    {
+        imageLink: 'images/eltern-abc-img.webp',
+        headingH1: '',
+        cardName: '',
+        cardNameDes: '',
+        cardList: {
+            listHeading: '',
+            listPoints: [
+                '',
+                ''
+            ],
+        },
+        cardFooterLine: '',
+    },
+];
+export default ElternComponentData;
