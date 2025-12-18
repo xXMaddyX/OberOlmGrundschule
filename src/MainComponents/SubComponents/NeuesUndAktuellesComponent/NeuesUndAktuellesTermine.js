a0dee4a4b8d29343072e0b28ef215d48d9399d7d
xXMaddyX <maddyaion@hotmail.de>
2025-12-18 17:31:51 +0100

    update owl

--- src/MainComponents/SubComponents/NeuesUndAktuellesComponent/NeuesUndAktuellesTermine.js
+++ src/MainComponents/SubComponents/NeuesUndAktuellesComponent/NeuesUndAktuellesTermine.js
@@ -5,22 +5,22 @@ const Termine = [
         {
             heading: "Theaterprojekt 2ab",
             date: "Mi. 17. Dez.",
-            discription: ""
+            discription: "",
         },
         {
             heading: "Weihnachtsfeier",
             date: "Fr. 19. Dez.",
-            discription: ""
+            discription: "",
         },
         {
             heading: "Weihnachtsferien",
             date: "Mo. 22. Dez. - Mi. 7. Jan. 2026",
-            discription: ""
+            discription: "",
         },
         {
             heading: "AG-Wahl",
             date: "Do. 15. Jan. 2026",
-            discription: ""
+            discription: "",
         },
         {
             heading: "Zeugniskonferenz",
@@ -109,6 +109,187 @@ const Termine = [
         {
             heading: "Hospitation Studienseminar Gymnasium",
             date: "Do. 12. Feb. 2026",
+            discription: ""
+        },
+    ],
+    [
+        //AB 14.FEB.2026 bis 02.Mär.2026
+        {
+            heading: "Schultreff Fastnachtsparty",
+            date: "Fr. 13. Feb. 2026",
+            discription: ""
+        },
+        {
+            heading: "Schulfrei",
+            date: "Mo. 16. Feb. 2026 - Mi. 18.",
+            discription: ""
+        },
+        {
+            heading: "Vorschulprojekt",
+            date: "Di. 24. Feb. 2026",
+            discription: ""
+        },
+        {
+            heading: "Open Stage international",
+            date: "Fr. 27. Feb. 2026",
+            discription: ""
+        },
+        {
+            heading: "Dienstbesprechung",
+            date: "Mo. 2. März 2026",
+            discription: ""
+        },
+    ],
+    [
+        //
+        {
+            heading: "Theaterstarter 2b",
+            date: "Fr. 6. März 2026",
+            discription: ""
+        },
+        {
+            heading: "Open Stage",
+            date: "Fr. 13. März 2026",
+            discription: ""
+        },
+        {
+            heading: "Forum Deutscher Schulpreis",
+            date: "Mo. 16. März 2026 - Di. 17.",
+            discription: ""
+        },
+        {
+            heading: "Känguru-Mathe-Wettbewerb",
+            date: "Do. 19. März 2026",
+            discription: ""
+        },
+        {
+            heading: "Singpause Frühlingslieder",
+            date: "Do. 19. März 2026",
+            discription: ""
+        },
+    ],
+    [
+        {
+            heading: "Gesamtkonferenz",
+            date: "Mo. 23. März 2026",
+            discription: ""
+        },
+        {
+            heading: "Schultreff",
+            date: "Mi. 25. März 2026",
+            discription: ""
+        },
+        {
+            heading: "Studientag",
+            date: "Do. 26. März 2026",
+            discription: ""
+        },
+        {
+            heading: "Osterferien",
+            date: "Fr. 27. März 2026 - Fr. 10. Apr.",
+            discription: ""
+        },
+        {
+            heading: "Abschluss-Fotos 4. Klassen",
+            date: "Di. 14. Apr. 2026",
+            discription: ""
+        },
+    ],
+    [
+        {
+            heading: "Open Stage Classic",
+            date: "Fr. 17. Apr. 2026",
+            discription: ""
+        },
+        {
+            heading: "Theaterstarter 2a",
+            date: "Di. 21. Apr. 2026",
+            discription: ""
+        },
+        {
+            heading: "Dienstbesprechung",
+            date: "Mo. 27. Apr. 2026",
+            discription: ""
+        },
+        {
+            heading: "Vera 3 lesen",
+            date: "Mo. 27. Apr. 2026",
+            discription: ""
+        },
+        {
+            heading: "Singpause Quatschlieder",
+            date: "Mi. 29. Apr. 2026 - Do. 30.",
+            discription: ""
+        },
+    ],
+    [
+        {
+            heading: "Vera 3 Mathe",
+            date: "Do. 30. Apr. 2026",
+            discription: ""
+        },
+        {
+            heading: "Schulfrei",
+            date: "Fr. 1. Mai 2026",
+            discription: ""
+        },
+        {
+            heading: "Mädchen-Fußballturnier Kreis Mainz-Bingen",
+            date: "Di. 12. Mai 2026",
+            discription: ""
+        },
+        {
+            heading: "Schulfrei",
+            date: "Do. 14. Mai 2026 - Fr. 15.",
+            discription: ""
+        },
+        {
+            heading: "Dienstbesprechung",
+            date: "Mo. 18. Mai 2026",
+            discription: ""
+        },
+    ],
+    [
+        {
+            heading: "Ausgleichstag Vera Schulfrei",
+            date: "Di. 26. Mai 2026",
+            discription: ""
+        },
+        {
+            heading: "Open Stage",
+            date: "Mi. 3. Juni 2026",
+            discription: ""
+        },
+        {
+            heading: "Schulfrei",
+            date: "Do. 4. Juni 2026 - Fr. 5.",
+            discription: ""
+        },
+        {
+            heading: "Zeugniskonferenz",
+            date: "Mo. 8. Juni 2026",
+            discription: ""
+        },
+        {
+            heading: "Schultreff mit Ehrungen",
+            date: "Fr. 12. Juni 2026",
+            discription: ""
+        },
+    ],
+    [
+        {
+            heading: "Singpause Sommerlieder",
+            date: "Fr. 19. Juni 2026",
+            discription: ""
+        },
+        {
+            heading: "Sommerferien",
+            date: "Mo. 29. Juni 2026 - Fr. 7. Aug.",
+            discription: ""
+        },
+        {
+            heading: "Theaterraum Mainz in der Schule",
+            date: "Fr. 19. Feb. 2027 - Sa. 20.",
             discription: ""
         },
     ]
